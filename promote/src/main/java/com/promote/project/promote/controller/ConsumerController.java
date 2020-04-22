5dd3b739391d46c720ab1b76299749f780998b77
Dennis <dennis.liu@tradevan.com.tw>
2020-04-22 16:12:41 +0800

    根據身分證查詢消費者 增加權限控制

--- promote/src/main/java/com/promote/project/promote/controller/ConsumerController.java
+++ promote/src/main/java/com/promote/project/promote/controller/ConsumerController.java
@@ -5,6 +5,7 @@ import com.promote.framework.web.domain.AjaxResult;
 import com.promote.project.promote.service.IConsumerService;
 import com.promote.project.system.domain.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class ConsumerController extends BaseController {
     @Autowired
     IConsumerService consumerService;
 
+    @PreAuthorize("@ss.hasRole('hostel')")
     @GetMapping("/identity/{identity}")
     public AjaxResult selectByIdentity(@PathVariable("identity") String identity) {
 
