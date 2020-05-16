7530166cf9da2d928bff52e74d8f2c80002ffd7e
6592 <peihuang.tseng@tradevan.com.tw>
2020-05-16 13:30:41 +0800

    改用roleid判斷角色

--- promote/src/main/java/com/promote/project/promote/controller/ProCommonController.java
+++ promote/src/main/java/com/promote/project/promote/controller/ProCommonController.java
@@ -446,26 +446,26 @@ public class ProCommonController extends BaseController {
     public AjaxResult updateProfile(@RequestBody SysUser sysUser) {
         SysUser user = SecurityUtils.getLoginUser().getUser();
         //判斷角色
-        String role = user.getRoles().get(0).getRoleKey();
+        Long roleId = user.getRoles().get(0).getRoleId();
         //登入者id
         Long userId = user.getUserId();
         //登入者帳號
         String username = user.getUsername();
         //資料類型
         String userType = null;
-        if("customerService".equals(role)){
+        if(RoleConstants.SERVICE_ROLE_ID.equals(roleId)){
             //客服
             userType = sysUser.getUserType();
             if(StringUtils.isEmpty(userType)){
                 return AjaxResult.error("需輸入資料類型");
             }
         }
-        if("hostel".equals(role)){
+        if(RoleConstants.HOTEL_ROLE_ID.equals(roleId)){
             //SA奕凱:目前不處理旅宿
-        }else if("store".equals(role) || ("customerService".equals(role) && "S".equals(userType))){
+        }else if(RoleConstants.STORE_ROLE_ID.equals(roleId) || (RoleConstants.SERVICE_ROLE_ID.equals(roleId) && "S".equals(userType))){
             //店家
             StoreInfo storeInfo = sysUser.getStore();
-            if("customerService".equals(role)){
+            if(RoleConstants.SERVICE_ROLE_ID.equals(roleId)){
                 if(StringUtils.isNull(storeInfo.getUserId()) || StringUtils.isEmpty(storeInfo.getUsername())){
                     return AjaxResult.error("需輸入userId及帳號");
                 }
@@ -476,10 +476,10 @@ public class ProCommonController extends BaseController {
             storeInfo.setUsername(username);
             storeService.updateStoreInfo(storeInfo);
             return AjaxResult.success("更新成功");
-        }else if("consumer".equals(role) || ("customerService".equals(role) && "C".equals(userType))){
+        }else if(RoleConstants.CONSUMER_ROLE_ID.equals(roleId) || (RoleConstants.SERVICE_ROLE_ID.equals(roleId) && "C".equals(userType))){
             //消費者
             ConsumerInfo consumer = sysUser.getConsumer();
-            if("customerService".equals(role)){
+            if(RoleConstants.SERVICE_ROLE_ID.equals(roleId)){
                 if(StringUtils.isNull(consumer.getUserId()) || StringUtils.isEmpty(consumer.getUsername())){
                     return AjaxResult.error("需輸入userId及帳號");
                 }
