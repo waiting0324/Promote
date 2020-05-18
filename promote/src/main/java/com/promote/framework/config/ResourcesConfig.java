c57ee11fa91cd9e7e09df53985174a66c23e5b29
Dennis <dennis.liu@tradevan.com.tw>
2020-05-18 17:00:34 +0800

    想辦法處理Cros問題

--- promote/src/main/java/com/promote/framework/config/ResourcesConfig.java
+++ promote/src/main/java/com/promote/framework/config/ResourcesConfig.java
@@ -2,6 +2,7 @@ package com.promote.framework.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -37,5 +38,13 @@ public class ResourcesConfig implements WebMvcConfigurer
     public void addInterceptors(InterceptorRegistry registry)
     {
         registry.addInterceptor(repeatSubmitInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
